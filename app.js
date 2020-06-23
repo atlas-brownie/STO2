ddd40c5c086d5c187a048f360dbd6ec6986c6c15
MBLDevSec2020 <67288848+MBLDevSec2020@users.noreply.github.com>
2020-06-23 11:57:55 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const http = require("http");
 const port = 8080;
 
 const server = http.createServer((req, res) => {
-  console.log(req);
-  
+  console.log(req.url);
+
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
   res.end("Hello World");
 });
 
 server.listen(port, () => {
-  console.log("Server running!");
+  console.log(`Server running on port ${port}`);
 });
