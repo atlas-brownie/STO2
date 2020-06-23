d9ccf34dd5458c4308ebf6e93601062259be72a8
MBLDevSec2020 <67288848+MBLDevSec2020@users.noreply.github.com>
2020-06-23 11:22:47 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const hostname = "127.0.0.1";
 const port = 8080;
 
 const server = http.createServer((req, res) => {
+  console.log(req);
+  
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
   res.end("Hello World");
